c901ee14a8ef4ed6dcfce6ab07d54372219499a2
areshero <834815821@qq.com>
2015-10-06 02:01:14 -0700

    Added handler for message body

--- hello.py
+++ hello.py
@@ -14,25 +14,40 @@ callers = {
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
+
+    message = handleMessage()
+
+    resp = twilio.twiml.Response()
+    resp.say('Hello monkey!!...... ')
+    resp.sms(message)
+    return str(resp)
+
+def handleMessage():
     counter = session.get('counter', 0)
     counter += 1
     session['counter'] = counter
- 
+
+    requestBody = handlerMessageBody()
+
     from_number = request.values.get('From')
-    requestBody = request.values.get('Body')
+    message = ""
     if from_number in callers:
         name = callers[from_number]
     else:
         name = "Monkey"
     if 'To' in request.values:
         messageTo = request.values.get('To')
-        message = "".join([requestBody," ", name, " has messaged ", messageTo, ", ", str(counter), " times."])
+        message = "".join(["Body: ", requestBody, " ...... ", name, " has messaged ", messageTo, ", ", str(counter), " times."])
     else:
         message = "error"
-    resp = twilio.twiml.Response()
-    resp.say('Hello monkey!!~~')
-    resp.sms(message)
-    return str(resp)
- 
+    return message
+
+def handlerMessageBody():
+    requestBody = request.values.get('Body')
+    requestList = requestBody.split(" ")
+    requestBodyResult = str(requestList[1]) + " " + str(requestList[3])
+
+    return requestBodyResult
+
 if __name__ == "__main__":
     app.run(debug=True)
