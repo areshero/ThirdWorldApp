b86a94b1f6313e164c1c729df02b04f872260587
areshero <834815821@qq.com>
2015-10-06 15:36:47 -0700

    Add a friend's number

--- hello.py
+++ hello.py
@@ -20,6 +20,7 @@ callers = {
 	"+14158675311": "Virgil",
 	"+12134001959": "SBLuke",
 	"+16412750872": "Yo Shen !"
+	"+17348348282": "Yo Yisha! Wha~~~up!"
 }
  
 @app.route("/", methods=['GET', 'POST'])
@@ -40,13 +41,14 @@ def handleMessage():
 	requestBody = handlerMessageBody()
 
 	from_number = request.values.get('From')
+	messageTo = request.values.get('To')
 	message = ""
 	if from_number in callers:
 		name = callers[from_number]
 	else:
 		name = "Monkey"
 	if 'To' in request.values:
-		messageTo = request.values.get('To')
+		
 		message = "".join(["Body: ", requestBody, " ...... ", name, " has messaged ", messageTo, ", ", str(counter), " times."])
 	else:
 		message = "error"
