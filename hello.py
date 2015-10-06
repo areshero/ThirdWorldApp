1e72946cc817eb42c6500f48f4d2d501743d0b1e
areshero <834815821@qq.com>
2015-10-06 01:19:24 -0700

    Add session

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, redirect, session
 import twilio.twiml
- 
-app = Flask(__name__)
 
+# The session object makes use of a secret key.
+SECRET_KEY = 'a secret key'
+app = Flask(__name__)
+app.config.from_object(__name__)
 # Try adding your own number to this list!
 callers = {
     "+14158675309": "Curious George",
@@ -12,19 +14,22 @@ callers = {
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
     counter = session.get('counter', 0)
     counter += 1
     session['counter'] = counter
-
+ 
     from_number = request.values.get('From')
     if from_number in callers:
         name = callers[from_number]
     else:
         name = "Monkey"
-    message = "".join([name, " has messaged ", request.values.get('To'), " ", str(counter), " times."])
+    if 'To' in request.values:
+        messageTo = request.values.get('To')
+        message = "".join([name, " has messaged ", messageTo, " ", str(counter), " times."])
+    else:
+        message = "error"
     resp = twilio.twiml.Response()
-    resp.message(message)
+    resp.sms(message)
     return str(resp)
  
 if __name__ == "__main__":
