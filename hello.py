ca8318bacbaa477f24866379edb6da8a4777d3dc
areshero <834815821@qq.com>
2015-10-06 01:10:28 -0700

    Added session

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import twilio.twiml
  
 app = Flask(__name__)
@@ -14,13 +13,16 @@ callers = {
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
- 
-    from_number = request.values.get('From', None)
-    if from_number in callers:
-        message = callers[from_number] + ", thanks for the message!"
-    else:
-        message = "Monkey, thanks for the message!"
+    counter = session.get('counter', 0)
+    counter += 1
+    session['counter'] = counter
 
+    from_number = request.values.get('From')
+    if from_number in callers:
+        name = callers[from_number]
+    else:
+        name = "Monkey"
+    message = "".join([name, " has messaged ", request.values.get('To'), " ", str(counter), " times."])
     resp = twilio.twiml.Response()
     resp.message(message)
     return str(resp)
